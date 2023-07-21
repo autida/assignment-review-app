bc21da31d78967279da5a52b0d553fadf95a0504
autida <autidaanalita@gmail.com>
2023-07-21 11:46:39 +0800

    Added Reviews

--- src/pages/components/Benefit.js
+++ src/pages/components/Benefit.js
@@ -1,7 +1,27 @@
 import React from "react";
 
 const Benefit = () => {
-  return <div></div>;
+  return (
+    <div className="b-container">
+      <div className="stars-wrapper">
+        <img
+          src="assets/image12.png"
+          alt="Jumbotron Background"
+          className="stars"
+        />
+      </div>
+      <div className="review-wrapper">
+        <h2 className="review-hdr">
+          1 in 3 Lorem ipsum dolor sit amet consectetur
+        </h2>
+        <h3 className="review-dtl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac
+          turpis a neque interdum euismod. Duis consectetur est a ipsum varius,
+          eu dictum odio tristique.
+        </h3>
+      </div>
+    </div>
+  );
 };
 
 export default Benefit;
