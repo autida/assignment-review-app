32186c239f03037fbe21225ba26989c573e268ad
autida <autidaanalita@gmail.com>
2023-08-03 10:47:52 +0800

    path update

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ import Service from "./pages/Service";
 const projectName = packageJson.name;
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path={projectName + "/"} element={<RootLayout />}>
+    <Route path={"/"} element={<RootLayout />}>
       <Route index element={<Home />} />
       <Route path={"/about"} element={<About />} />
       <Route path={"/service"} element={<Service />} />
